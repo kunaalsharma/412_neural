7b1c7a4131fccd1bde0f790430895b184b1ecb70
jjt20 <38593373+jjt20@users.noreply.github.com>
2018-04-24 04:14:05 -0400

    Calculates VR filtration

---
+++ EireneCalc.jl
@@ -0,0 +1,29 @@
+dist0 = ezread("/Users/joetharayil/Documents/Topology/PH-roadmap-master/matlab/412_neural-master/weightedAdjacencyMatrices/Save1wb0_edge_list_distmat.txt");
+C0 = eirene(dist0,bettimax=2)
+
+dist1 = ezread("/Users/joetharayil/Documents/Topology/PH-roadmap-master/matlab/412_neural-master/weightedAdjacencyMatrices/Save1wb1_edge_list_distmat.txt");
+C1 = eirene(dist1,bettimax=2)
+
+dist2 = ezread("/Users/joetharayil/Documents/Topology/PH-roadmap-master/matlab/412_neural-master/weightedAdjacencyMatrices/Save1wb2_edge_list_distmat.txt");
+C2 = eirene(dist2,bettimax=2)
+
+dist3 = ezread("/Users/joetharayil/Documents/Topology/PH-roadmap-master/matlab/412_neural-master/weightedAdjacencyMatrices/Save1wb3_edge_list_distmat.txt");
+C3 = eirene(dist3,bettimax=2)
+
+dist4 = ezread("/Users/joetharayil/Documents/Topology/PH-roadmap-master/matlab/412_neural-master/weightedAdjacencyMatrices/Save1wb4_edge_list_distmat.txt");
+C4 = eirene(dist4,bettimax=2)
+
+dist5 = ezread("/Users/joetharayil/Documents/Topology/PH-roadmap-master/matlab/412_neural-master/weightedAdjacencyMatrices/Save1wb5_edge_list_distmat.txt");
+C5 = eirene(dist5,bettimax=2)
+
+dist6 = ezread("/Users/joetharayil/Documents/Topology/PH-roadmap-master/matlab/412_neural-master/weightedAdjacencyMatrices/Save1wb6_edge_list_distmat.txt");
+C6 = eirene(dist6,bettimax=2)
+
+dist7 = ezread("/Users/joetharayil/Documents/Topology/PH-roadmap-master/matlab/412_neural-master/weightedAdjacencyMatrices/Save1wb7_edge_list_distmat.txt");
+C7 = eirene(dist7,bettimax=2)
+
+dist8 = ezread("/Users/joetharayil/Documents/Topology/PH-roadmap-master/matlab/412_neural-master/weightedAdjacencyMatrices/Save1wb8_edge_list_distmat.txt");
+C8 = eirene(dist8,bettimax=2)
+
+dist9 = ezread("/Users/joetharayil/Documents/Topology/PH-roadmap-master/matlab/412_neural-master/weightedAdjacencyMatrices/Save1wb9_edge_list_distmat.txt");
+C9 = eirene(dist9,bettimax=2)
